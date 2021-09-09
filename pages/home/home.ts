2b795d5d602048cad85bb5bb61a4d4bc3107c6d5
addisudamena <addisudamena49@gmail.com>
2021-09-08 23:42:30 -0600

    1 file modified, 2 added

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {XMLData} from 'source.xml';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
 
+var rawFile = new XMLHttpRequest();
+rawFile.open("GET", XMLData, false);
+rawFile.onreadystatechange = () => {
+    if (rawFile.readyState === 4) {
+        if (rawFile.status === 200 || rawFile.status == 0) {
+            var xmlasstring = rawFile.responseText;
+            console.log('Your xml file as string', xmlasstring)
+        }
+    }
   }
 
+export class HomePage {
+  constructor(public navCtrl: NavController) {}
+
+
+
+
+  /*
+
+import XMLData from '../../xmldata/list.xml';
+
+axios.get(XMLData, {
+   "Content-Type": "application/xml; charset=utf-8"
+})
+.then((response) => {
+   console.log('Your xml file as string', response.data);
+});
+*/
 }
